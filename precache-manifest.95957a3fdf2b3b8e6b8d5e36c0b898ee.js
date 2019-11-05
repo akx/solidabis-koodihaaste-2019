62b1d3f219e6bc77ae33781f9d7e11bfd7cb30d3
Aarni Koskela <akx@iki.fi>
2019-11-05 10:42:25 -0200

    f1a5b720a64ba14a748616cd99a3ca3aca09e787

---
+++ precache-manifest.95957a3fdf2b3b8e6b8d5e36c0b898ee.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a590c21eee17323949ddf48cc4aa5224",
+    "url": "/solidabis-koodihaaste-2019/index.html"
+  },
+  {
+    "revision": "0555f23695528e9cc5c9",
+    "url": "/solidabis-koodihaaste-2019/static/css/main.fd0ff5ab.chunk.css"
+  },
+  {
+    "revision": "8f62fe906559501d2c65",
+    "url": "/solidabis-koodihaaste-2019/static/js/2.6d0262cb.chunk.js"
+  },
+  {
+    "revision": "0555f23695528e9cc5c9",
+    "url": "/solidabis-koodihaaste-2019/static/js/main.6468343f.chunk.js"
+  },
+  {
+    "revision": "fe575a78d11b9f7e40cf",
+    "url": "/solidabis-koodihaaste-2019/static/js/runtime-main.282fa95b.js"
+  }
+]);
